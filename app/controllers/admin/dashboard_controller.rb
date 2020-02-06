01e6206bdce240c12c085894a5d319c0f6838ab0
Prateek Diwedi <diwedi1112@gmail.com>
2020-02-06 03:34:04 +0000

    updated code now admin can access dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['username'].to_s, password: ENV['username'].to_s
+  
   def show
+    @product_count = Product.sum(:quantity)
+    @category_count = Category.count
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with :name => "jungle", :password => "book" 
+  http_basic_authenticate_with name: ENV['username'].to_s, password: ENV['password'].to_s
 
 
   def index
