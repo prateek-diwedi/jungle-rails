d499097fe14c1dd46853e0d67476ad118af24266
Prateek Diwedi <diwedi1112@gmail.com>
2020-02-07 01:01:10 +0000

    updated code for login logout button and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   resource :about, only: [:show] 
   root to: 'products#index'
 
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
