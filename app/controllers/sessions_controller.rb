238b9bba5285c0ceab242319085546c80360f1ed
Prateek Diwedi <diwedi1112@gmail.com>
2020-02-11 02:58:33 +0000

    created user test rspec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,4 +26,15 @@ class SessionsController < ApplicationController
  
   end
 
+  def create
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
+      # success logic, log them in
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      # failure, render login form
+      redirect_to '/login'
+    end
+  end
+
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,43 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
-    let(:category) { Category.new(name: "electronics") }
-
-    subject { described_class.new(name: "Television", price_cents: 1000000, quantity: 25, category: category) }
-
-    it "is valid with all valid attributes" do
-      expect(subject).to be_valid
-      subject.save
-      expect(subject.errors.full_messages).to eql([])
+    it 'should create a product if all of the validations are true' do
+    @category = Category.new(name: "Testing")
+    @product = Product.new(name: "Supreme Nas Tee", price_cents: 3500, quantity: 35, :category => @category)
+    @product.valid?
+    expect(@product.errors.full_messages).not_to eql("can\'t be blank")
     end
 
-    it "is not valid without a name" do
-        subject.name = nil
-        expect(subject).not_to be_valid
-        subject.save
-        expect(subject.errors.full_messages).to eql(["Name can't be blank"])
+    it 'should not create a product if the name is missing' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(price_cents: 3500, quantity: 35, :category => @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to eql(["Name can\'t be blank"])
     end
 
-    it "is not valid without a price" do
-      subject.price_cents = nil
-      expect(subject).not_to be_valid
-      subject.save
-      expect(subject.errors.full_messages).to eql(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+    it 'should not create a product if the quantity is missing' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(name: "Supreme Nas Tee", price_cents: 3500, :category => @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to eql(["Quantity can\'t be blank"])
     end
 
-    it "is not valid without a quantity" do
-      subject.quantity = nil
-      expect(subject).not_to be_valid
-      subject.save
-      expect(subject.errors.full_messages).to eql(["Quantity can't be blank"])
+    it 'should not create a product if the price is missing' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(name: "Supreme Nas Tee", quantity: 35, :category => @category)
+      @product.valid?
+      expect(@product.errors.full_messages).to eql(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
     end
 
-    it "is not valid without a category" do
-      subject.category = nil
-      expect(subject).not_to be_valid
-      subject.save
-      expect(subject.errors.full_messages).to eql(["Category can't be blank"])
+    it 'should not create a product if the category is missing' do
+      @category = Category.new(name: "Testing")
+      @product = Product.new(name: "Supreme Nas Tee", price_cents: 3500, quantity: 35)
+      @product.valid?
+      expect(@product.errors.full_messages).to eql(["Category can\'t be blank"])
     end
   end
 end
